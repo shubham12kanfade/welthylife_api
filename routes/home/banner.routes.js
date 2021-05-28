9530424a0d024de0027ebc06bc8054722c60b711
nishitgarg42 <nishitgarg42@gmail.com>
2021-05-28 14:29:06 +0530

    initial commit

--- routes/home/banner.routes.js
+++ routes/home/banner.routes.js
@@ -18,4 +18,55 @@ router.get("/all", (req, res) => {
       });
   });
 
+  router.get("/by/bannerId/:id", (req, res) => {
+    log.debug("/api/");
+    crudController
+    .getBy(Banner, { _id: req.params.id })
+    .then((testData) => {
+      response.successResponse(res, 200, testData);
+    })
+    .catch((error) => {
+      log.error(error);
+      response.errorResponse(res, 500);
+    });
+  });
+
+  router.post("/add", (req, res) => {
+    log.debug("/api/");
+    crudController
+      .add(Banner, req.body)
+      .then((testData) => {
+        response.successResponse(res, 200, testData);
+      })
+      .catch((error) => {
+        log.error(error);
+        response.errorResponse(res, 500);
+      });
+  });
+
+  router.delete("/bannerId/:id", (req, res) => {
+    log.debug("/api/");
+    crudController
+      .delete(Banner, req.params.id)
+      .then((userData) => {
+        response.successResponse(res, 200, userData);
+      })
+      .catch((error) => {
+        log.error(error);
+        response.errorResponse(res, 500);
+      });
+  });
+
+  router.put("/bannerId/:id", (req, res) => {
+    crudController
+      .updateBy(Banner, req.params.id, req.body)
+      .then((userData) => {
+        response.successResponse(res, 200, userData);
+      })
+      .catch((error) => {
+        log.error(error);
+        response.errorResponse(res, 500);
+      });
+  });
+
   module.exports = router;
